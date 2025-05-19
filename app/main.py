3e020dda56bd0bcef388deff81faef4620910d04
Andrii Omelchenko <andreyomem@gmail.com>
2025-05-19 16:17:22 +0300

    implement copy_file

--- app/main.py
+++ app/main.py
@@ -1,1 +1,11 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_list = command.split(" ")
+    if command_list[0] == "cp" and len(command_list) == 3:
+        if command_list[1] != command_list[2]:
+            try:
+                with (open(command_list[1], "r") as file_input,
+                      open(command_list[2], "w") as file_output):
+                    for line in file_input:
+                        file_output.write(line)
+            except FileNotFoundError:
+                print("File not found")
